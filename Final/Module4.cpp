23ed608db4674fa0bfc06454902e5bfa0f3c436e
SherlockHowl <60418871+SherlockHowl@users.noreply.github.com>
2020-04-03 11:41:50 +0530

    Add files via upload

--- Final/Module4.cpp
+++ Final/Module4.cpp
@@ -96,9 +96,7 @@ vector <int> Test_articulationpoints(Graph G)
             if(VertexArray[u].color==1)
                 cou++;
         }
-        if(cou==(G.V-1))
-            cout<<i<<" is not an AP"<<endl;
-        else
+        if(cou!=(G.V-1))
         {
             ap.push_back(i);
             couAP++;
@@ -106,4 +104,59 @@ vector <int> Test_articulationpoints(Graph G)
     }
     ap.insert(ap.begin(),couAP);
     return ap;
+}
+vector <pair<int,int>> Test_bridges(Graph G)
+{
+    Vertex VertexArray[20];
+    queue <int> q;
+    Graph T(G.V,G.E); // temporary graph
+    int couB=0;
+    vector <pair<int,int>> bridge;
+    for(int i=0;i<G.V;i++)
+    {
+        for(auto j=G.adj[i].begin();j<G.adj[i].end();j++)
+        {
+            int k=j->first;
+            T=G;
+            for(auto x=T.adj[i].begin();x<T.adj[i].end();x++)
+            {
+                if(x->first==k)
+                    T.adj[i].erase(x);
+            }
+            for(auto y=T.adj[k].begin();y<T.adj[k].end();y++)
+            {
+                if(y->first==i)
+                    T.adj[k].erase(y);
+            }
+            for(int a=0;a<20;a++)
+                VertexArray[a].color=0;
+            q.push(0); // bfs starts
+            VertexArray[0].color=1;
+            while(!q.empty())
+            {
+                int t=q.front();
+                q.pop();
+                for(auto k=T.adj[t].begin();k<T.adj[t].end();k++)
+                {
+                    if(VertexArray[k->first].color==0)
+                    {
+                        q.push(k->first);
+                        VertexArray[k->first].color=1;
+                    }
+                }
+            } // bfs ends
+            int cou=0;
+            for(int u=0;u<G.V;u++)
+            {
+                if(VertexArray[u].color==1)
+                    cou++;
+            }
+            if(cou!=G.V)
+            {
+                bridge.push_back(make_pair(i,k));
+                couB++;
+            }
+        }
+    }
+    return bridge;
 }
--- Final/Module4.h
+++ Final/Module4.h
@@ -2,3 +2,4 @@
 using namespace std;
 bool Test_bipartite(Graph G);
 vector <int> Test_articulationpoints(Graph G);
+vector <pair<int,int>> Test_bridges(Graph G);
