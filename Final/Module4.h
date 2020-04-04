30f0d48f00e7e8b79310b63b2f8b29246a8a7afe
T Karthikeyan <49520037+tkarthikeyan132@users.noreply.github.com>
2020-04-04 15:50:59 +0530

    Update Module4.h

--- Final/Module4.h
+++ Final/Module4.h
@@ -1,5 +1,5 @@
 #include"GraphSTL.h"
 using namespace std;
-bool Test_bipartite(Graph G);
-vector <int> Test_articulationpoints(Graph G);
-vector <pair<int,int>> Test_bridges(Graph G);
+bool Test_bipartite(Graph &G);
+vector <int> Test_articulationpoints(Graph &G);
+vector <pair<int,int>> Test_bridges(Graph &G);
