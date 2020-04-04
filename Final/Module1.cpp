f4d417a73be0415087203bbb9b9f855e218c0014
T Karthikeyan <49520037+tkarthikeyan132@users.noreply.github.com>
2020-04-04 13:43:40 +0530

    Update Module1.cpp

--- Final/Module1.cpp
+++ Final/Module1.cpp
@@ -4,7 +4,7 @@ void BFS(Graph &G)
 {
     queue <int> q;
     q.push(0);
-    Vertex_Array[0].color=1;
+    G.Vertex_Array[0].color=1;
     while(!q.empty())
     {
         int t=q.front();
@@ -12,10 +12,10 @@ void BFS(Graph &G)
         cout<<t<<" ";
         for(auto k=G.adj[t].begin();k<G.adj[t].end();k++)
         {
-            if(Vertex_Array[k->first].color==0)
+            if(G.Vertex_Array[k->first].color==0)
             {
                 q.push(k->first);
-                Vertex_Array[k->first].color=1;
+                G.Vertex_Array[k->first].color=1;
             }
         }
     }
@@ -25,9 +25,9 @@ Graph BFS(Graph &G, int s)
     Graph T(G.V,G.V-1);
     queue <int> q;
     q.push(s);
-    Vertex_Array[s].color=1;
-    Vertex_Array[s].pid=-1; //NULL
-    Vertex_Array[s].dist=0;
+    G.Vertex_Array[s].color=1;
+    G.Vertex_Array[s].pid=-1; //NULL
+    G.Vertex_Array[s].dist=0;
     while(!q.empty())
     {
         int t=q.front();
@@ -35,17 +35,17 @@ Graph BFS(Graph &G, int s)
         cout<<t<<" ";
         for(auto k=G.adj[t].begin();k!=G.adj[t].end();k++)
         {
-            if(Vertex_Array[k->first].color==0)
+            if(G.Vertex_Array[k->first].color==0)
             {
                 q.push(k->first);
-                Vertex_Array[k->first].pid=t;
-                Vertex_Array[k->first].dist=Vertex_Array[t].dist+1;
-                Vertex_Array[k->first].color=1;
+                G.Vertex_Array[k->first].pid=t;
+                G.Vertex_Array[k->first].dist=G.Vertex_Array[t].dist+1;
+                G.Vertex_Array[k->first].color=1;
                 if(G.directed==0)
                 T.addUEdge(t,k->first,k->second);
                 else
                 T.addEdge(t,k->first,k->second);
-                Vertex_Array[k->first].color=1;
+                G.Vertex_Array[k->first].color=1;
             }
         }
     }
@@ -55,7 +55,7 @@ bool BFS(Graph &G,int s,int r)
 {
     queue <int> q;
     q.push(s);
-    Vertex_Array[s].color=1;
+    G.Vertex_Array[s].color=1;
     while(!q.empty())
     {
         int t=q.front();
@@ -64,10 +64,10 @@ bool BFS(Graph &G,int s,int r)
             return true;
         for(auto k=G.adj[t].begin();k<G.adj[t].end();k++)
         {
-            if(Vertex_Array[k->first].color==0)
+            if(G.Vertex_Array[k->first].color==0)
             {
                 q.push(k->first);
-                Vertex_Array[k->first].color=1;
+                G.Vertex_Array[k->first].color=1;
             }
         }
     }
