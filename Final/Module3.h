a6b2f688fd671224e3f94b4fb39dd73b13a7bb16
Animesh241100 <60282715+Animesh241100@users.noreply.github.com>
2020-04-04 21:09:25 +0530

    put maxmst in module3.h
    @SherlockHowl , you had not put it into .h but directly put into .cpp file.

--- Final/Module3.h
+++ Final/Module3.h
@@ -5,3 +5,4 @@ vector<int> LPATH(Graph &G, int flag);
 vector<int> topological_ordering(Graph &G);
 vector<int> DAG_LONGEST_PATH(Graph &G, Vertex s, vector<int> Topological_vector);
 vector<vector<int>> BFS_SPATH(Graph G, Vertex s, int flag);
+Graph MAX_Weight_MST(Graph G);   //a part of module5 is done here and it has a purpose.
