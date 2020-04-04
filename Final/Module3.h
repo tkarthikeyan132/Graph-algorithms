2eb579628693015b7e7f45e8df5d548e31ccb2a4
Animesh241100 <60282715+Animesh241100@users.noreply.github.com>
2020-04-04 15:28:07 +0530

    removed initialise for  bfs

--- Final/Module3.h
+++ Final/Module3.h
@@ -2,6 +2,5 @@
 using namespace std;
 
 vector<int> LPATH(Graph &G, int flag);
-void Initialize_for_BFS(Graph &G, int s);
 vector<int> topological_ordering(Graph &G);
 vector<int> DAG_LONGEST_PATH(Graph &G, Vertex s, vector<int> Topological_vector);
